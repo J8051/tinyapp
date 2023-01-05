30db32938a053cdd99b2937d3e9bf92135720379
J8051 <nathaniel.jessica@yahoo.ca>
2023-01-04 20:34:47 -0500

    added form and post request

--- express_server.js
+++ express_server.js
@@ -10,6 +10,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 }; 
 
+function generateRandomString() {
+  let sixCharacterUniqueId = ""; 
+  let characters = "abcdefghijklmnopqrstuvwxyz123456789";
+  let charArr = characters.split("");
+  for (let i = 0; i <6; i++) { 
+    sixCharacterUniqueId += charArr[Math.round(Math.random() * charArr.length)];  
+  };
+  return sixCharacterUniqueId; 
+}
+console.log(generateRandomString()); 
+
 app.get("/", (req, res) => {
   res.send("Hello");
 }); 
@@ -23,11 +34,11 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  
   const templateVars = { urls: urlDatabase }; 
   console.log("test log");
   res.render("urls_index", templateVars); 
 }); 
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -40,6 +51,10 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 }); 
 
+app.post("/urls", (req, res) => { 
+  console.log(req.body)// Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`); 
