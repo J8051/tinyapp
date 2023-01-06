1901511ce8b4c18f73a47696cbd4b677270f77e0
J8051 <nathaniel.jessica@yahoo.ca>
2023-01-05 19:29:16 -0500

    created functioning edit button

--- express_server.js
+++ express_server.js
@@ -1,5 +1,5 @@
-const express = require("express"); 
-const app = express(); 
+const express = require("express");
+const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
@@ -9,36 +9,36 @@ app.use(express.urlencoded({ extended: true }));
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-}; 
+};
 
 
 function generateRandomString() {
-  let sixCharacterUniqueId = ""; 
+  let sixCharacterUniqueId = "";
   let characters = "abcdefghijklmnopqrstuvwxyz123456789";
   let charArr = characters.split("");
-  for (let i = 0; i <6; i++) { 
-    sixCharacterUniqueId += charArr[Math.round(Math.random() * charArr.length)];  
+  for (let i = 0; i < 6; i++) {
+    sixCharacterUniqueId += charArr[Math.round(Math.random() * charArr.length)];
   };
-  return sixCharacterUniqueId; 
+  return sixCharacterUniqueId;
 }
 
 //Routes 
 app.get("/", (req, res) => {
   res.send("Hello");
-}); 
+});
 
-app.get("/urls.json", (req, res) => { 
-  res.json(urlDatabase); 
-})
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase }; 
-  res.render("urls_index", templateVars); 
-}); 
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -47,35 +47,41 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
-  console.log(req.params);
   const longURL = urlDatabase[id];
-  const templateVars = { id: req.params.id, longURL: longURL};
+  const templateVars = { id, longURL };
+  console.log(templateVars);
   res.render("urls_show", templateVars);
-}); 
+});
 
-app.post("/urls", (req, res) => { 
-  const shortUrl = generateRandomString(); 
+app.post("/urls", (req, res) => {
+  const shortUrl = generateRandomString();
   urlDatabase[shortUrl] = req.body.longURL;
-  console.log(urlDatabase); 
+  console.log(urlDatabase);
   //console.log(req.body)// Log the POST request body to the console
-    res.redirect(`/urls/${shortUrl}`);
+  res.redirect(`/urls/${shortUrl}`);
   //res.send("ok"); // Respond with 'Ok' (we will replace this)
-})
+});
 
-app.post("/urls/:id/delete", (req, res) => { 
+app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
-
-    res.redirect("/urls");
-
-})
+  res.redirect("/urls");
+});
 
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
 
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = req.body.longURL;
+  urlDatabase[id] = longURL;
+
+  res.redirect("/urls");
+});
+
 //Server Listening 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`); 
+  console.log(`Example app listening on port ${PORT}!`);
 }); 
